5e5ba46dd6a93ff8441fb52ba4090d11acd7d3a3
Thiri Thaw <thiri12505@gmail.com>
2025-06-23 15:28:08 +0000

    2.1

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,1 +1,11 @@
-
+#!/usr/bin/env python3
+try:
+    num = int(input("Enter number: "))
+    if num > 0:
+        print("This number is positive")
+    elif num < 0:
+        print("This number is negative")
+    elif num == 0:
+        print("This number is both positive and negative")
+except ValueError:
+    print("Invalid! Please enter number only")
