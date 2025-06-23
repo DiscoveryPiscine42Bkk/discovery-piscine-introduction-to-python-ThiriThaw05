b9a923719a8726d15d2f751852d32e53e39c3cbe
Thiri Thaw <thiri12505@gmail.com>
2025-06-23 14:10:19 +0000

    2.0

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -1,1 +1,9 @@
-
+#!/usr/bin/env python3
+try:
+    num = int(input("Enter number: "))
+    if num == 0:
+        print("This number is equal to zero")
+    else:
+        print("This number is different from zero")
+except ValueError:
+    print("Invalid! Please enter number only")
